c8e10e1ecfcd12f191b7ae2c049e76ddd9219a87
Zokyn <gustavo.casaes@mail.uft.edu.br>
2023-03-21 15:06:42 -0300

    criando pequenos dataframe com valores max e mins

--- main.py
+++ main.py
@@ -2,20 +2,42 @@
 import pandas as pd
 dataframe = pd.read_excel("dataset.xlsx")
 # print(dataframe)
-POSITIVES = []
-REGULAR_WARD = []
-SEMI_INTENSIVE = []
-INTENSIVE_CARE = []
 
+POSITIVES = []
+INTENSIVE_CARE = []
 for header in dataframe:
     # column = pd.Series(dataframe[header])
     if(header == 'SARS-Cov-2 exam result'):
-        POSITIVES = dataframe[dataframe['SARS-Cov-2 exam result'] == 'positive']
-    if(
-        header == 'Patient addmited to regular ward (1=yes, 0=no)' or
-        header == 'Patient addmited to semi-intensive unit (1=yes, 0=no)' or
-        header == 'Patient addmited to intensive care unit (1=yes, 0=no)'
-        ):
-        REGULAR_WARD = dataframe[dataframe['Patient addmited to regular ward (1=yes, 0=no)'] == 1]
-        SEMI_INTENSIVE = dataframe[dataframe['Patient addmited to semi-intensive unit (1=yes, 0=no)'] == 1]
-        INTENSIVE_CARE = dataframe[dataframe['Patient addmited to intensive care unit (1=yes, 0=no)'] == 1]
+        POSITIVES = dataframe[dataframe[header] == 'positive']
+    if(header == 'Patient addmited to intensive care unit (1=yes, 0=no)'):
+        INTENSIVE_CARE = dataframe[dataframe[header] == 1]
+
+NUMERIC_FIELDS = [
+    'Hematocrit',
+    'Hemoglobin',
+    'Platelets',
+    'Mean platelet volume ',
+    'Red blood Cells',
+    'Lymphocytes',
+    'Mean corpuscular hemoglobin concentration\xa0(MCHC)',
+    'Leukocytes',
+    'Basophils',
+    'Mean corpuscular hemoglobin (MCH)',
+    'Eosinophils',
+    'Mean corpuscular volume (MCV)',
+    'Monocytes',
+    'Red blood cell distribution width (RDW)',
+    'Serum Glucose'
+]        
+for field in NUMERIC_FIELDS:
+    name = field
+    data = dataframe[field].dropna()
+
+    table = pd.DataFrame({
+        "value" : pd.Series(data.reset_index(drop=True)),
+        "max" : pd.Series([data.max()]*data.shape[0]),
+        "min" : pd.Series([data.min()]*data.shape[0])
+    })
+    f = open(f"./data/{name}.txt", "w")
+    f.write(str(table.T.to_string()))
+    f.close()
